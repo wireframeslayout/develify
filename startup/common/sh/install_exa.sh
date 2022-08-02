a80e0c6e3492c61fe623fbf47a41e5e6aef183b9
junzomatunoo <no.diggi.t@gmail.com>
2022-08-02 13:48:22 +0900

    fix exa install

--- startup/common/sh/install_exa.sh
+++ startup/common/sh/install_exa.sh
@@ -6,7 +6,7 @@ if [[ $(command -v exa) ]]; then
 else
     echo "install exa"
     wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip
-    sudo unzip exa-linux-x86_64-v0.10.0.zip -d ~/exa
+    unzip exa-linux-x86_64-v0.10.0.zip -d ~/exa
     sudo mv ~/exa/bin/exa /usr/local/bin/exa
     rm -rf ~/exa
 fi
